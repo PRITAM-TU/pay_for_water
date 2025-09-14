b5ceacc61f736240fb7424f931fa468fbd980630
Pritam Tung <pritamtung03@gmail.com>
2025-09-14 22:56:45 +0530

    Add Gunicorn for production

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def submit_payment():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+
+    app.run(host='0.0.0.0', port=5000, debug=False)
